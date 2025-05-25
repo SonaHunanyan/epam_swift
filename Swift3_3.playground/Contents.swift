358372c8128a4d1e28a2b147ebe67eb13ff78b96
SonaHunanyan <hunanyanson@gmail.com>
2025-05-25 21:04:34 +0400

    Add Swift3_3

---
+++ Swift3_3.playground/Contents.swift
@@ -0,0 +1,26 @@
+let books = [
+["title": "Swift Fundamentals", "author": "John Doe", "year": 2015, "price": 40, "genre": ["Programming", "Education"]],
+["title": "The Great Gatsby", "author": "F. Scott Fitzgerald", "year": 1925, "price": 15, "genre": ["Classic", "Drama"]],
+["title": "Game of Thrones", "author": "George R. R. Martin", "year": 1996, "price": 30, "genre": ["Fantasy", "Epic"]],
+["title": "Big Data, Big Dupe", "author": "Stephen Few", "year": 2018, "price": 25, "genre": ["Technology", "Non-Fiction"]],
+["title": "To Kill a Mockingbird", "author": "Harper Lee", "year": 1960, "price": 20, "genre": ["Classic", "Drama"]]
+]
+
+
+var discountedPrices: [Double] = books.compactMap( { book in
+    if let price = book["price"] as? Int{
+        let doublePrice = Double(price)
+        return doublePrice - doublePrice * 0.1
+    }
+    return nil
+})
+
+
+var booksPostedAfter2000: [String] = books.filter( { $0["year"] as! Int > 2000 } ).map( { $0["title"] as! String } )
+
+
+var allGenres: Set<String> = Set(books.flatMap( { $0["genre"] as! [String] } ))
+
+var totalCost: Int = books.compactMap( { $0["price"] as? Int} ).reduce(0, { $0 + $1 })
+
+
