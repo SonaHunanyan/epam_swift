9a936b9ed3b8a3613e23c8042c00e2a615dfba48
SonaHunanyan <hunanyanson@gmail.com>
2025-07-30 20:54:17 +0400

    Fix AsyncAwaitFinalTask 2

--- AsyncAwaitFinalTask/AsyncAwaitFinalTask/Task2/Task2View.swift
+++ AsyncAwaitFinalTask/AsyncAwaitFinalTask/Task2/Task2View.swift
@@ -9,11 +9,16 @@ import SwiftUI
 
 struct Task2View: View {
     let task2API: Task2API = .init()
-
+    
     @State var user: Task2API.User?
     @State var products: [Task2API.Product] = []
     @State var duration: TimeInterval?
-
+    
+    enum FetchResult {
+        case user(Task2API.User)
+        case products([Task2API.Product])
+    }
+    
     var body: some View {
         VStack {
             if let user, !products.isEmpty, let duration {
@@ -26,35 +31,51 @@ struct Task2View: View {
                 Text("Loading in progress")
             }
         }.task {
-            do {
                 let startDate = Date.now
-                user = try await task2API.getUser()
-                products = try await task2API.getProducts()
+                await withThrowingTaskGroup(of: FetchResult.self) { taskGroup in
+                    taskGroup.addTask {
+                        let result = try await task2API.getUser()
+                        return .user(result)
+                    }
+                    taskGroup.addTask {
+                        let result = try await task2API.getProducts()
+                        return .products(result)
+                    }
+                    
+                    do {
+                        for try await value in taskGroup {
+                            switch value {
+                            case .user(let value):
+                                self.user = value
+                            case .products(let value):
+                                self.products = value
+                            }
+                        }
+                    } catch {
+                        print(error)
+                    }
+                }
                 let endDate = Date.now
-
                 duration = DateInterval(start: startDate, end: endDate).duration
-            } catch {
-                print("unexpected error")
             }
         }
-    }
 }
 
 class Task2API: @unchecked Sendable {
     struct User {
         let name: String
     }
-
+    
     struct Product: Identifiable {
         let id: String
         let description: String
     }
-
+    
     func getUser() async throws -> User {
         try await Task.sleep(for: .seconds(1))
         return .init(name: "John Smith")
     }
-
+    
     func getProducts() async throws -> [Product] {
         try await Task.sleep(for: .seconds(1))
         return [.init(id: "1", description: "Some cool product"),
