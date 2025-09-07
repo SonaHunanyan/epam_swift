09f23f82cc28dedfd59be68a5297ee484a32ff40
SonaHunanyan <hunanyanson@gmail.com>
2025-09-07 14:40:03 +0400

    Add final project

---
+++ EpamFinal/EpamFinal/EpamFinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  EpamFinalApp.swift
+//  EpamFinal
+//
+//  Created by Sona Hunanyan on 07.09.25.
+//
+
+import SwiftUI
+
+@main
+struct EpamFinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
---
+++ EpamFinal/EpamFinal/Features/Home/View/HomeView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EpamFinal
+//
+//  Created by Sona Hunanyan on 07.09.25.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    HomeView()
+}
